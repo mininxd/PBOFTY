e8539bb9fc06b662bac80c81f3f0cbd73292ce64
Minin <70429604+MininxD@users.noreply.github.com>
2023-08-15 00:58:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+//time target
+var date1 = new Date("01/01/2023");
+//time now
+var date2 = new Date();
+//get time of hour
+var timeGet = date2.getTime() - date1.getTime();
+//set into days
+var Difference_In_Days = timeGet / (1000 * 3600 * 24);
+
+//progress line
+const progress = document.getElementById("progress");
+//percentage of days
+const percent = document.getElementById("days_total");
+//disable decimals of days total
+const total = Math.trunc(Difference_In_Days);
+
+//set interval for declaring progress and %
+const interval = setInterval(function () {
+  //information of %
+  percent.innerHTML = Math.trunc((total / "365") * "100") + "%";
+
+  //progress
+  progress.style.width = Math.trunc((total / "365") * "100") + "%";
+
+  //interval time
+}, 100);
