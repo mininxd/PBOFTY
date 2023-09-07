2bc44a68de6c2f07866b185d54186785a75aa2bd
Minin <70429604+MininxD@users.noreply.github.com>
2023-09-07 09:33:17 +0700

    add interval for declare live time

--- script.js
+++ script.js
@@ -1,79 +1,77 @@
-fetch("https://timeapi.mininxd.my.id/", {
-  header: {
-    "Content-Type": "Application/JSON",
-  },
-})
-  .then((res) => {
-    return res.json();
+function time() {
+  for (i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+
+var x = setInterval(function () {
+  fetch("https://timeapi.mininxd.my.id/", {
+    header: {
+      "Content-Type": "Application/JSON",
+    },
   })
-  .then((data) => {
-    console.log(data);
-    const serverdate = data.wib[0].date;
-    const serverday = data.wib[0].weekday;
-    const servertime = data.wib[0].time;
-    const serveryear = data.wib[0].year;
-    const servertime12h = data.wib[0].time12;
-    const servertime24h = data.wib[0].time24;
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const serverdate = data.wib[0].date;
+      const serverday = data.wib[0].weekday;
+      const servertime = data.wib[0].time;
+      const serveryear = data.wib[0].year;
+      const servertime12h = data.wib[0].time12;
+      const servertime24h = data.wib[0].time24;
 
-    //leap year check
-    let leap = new Date(data.wib[0].year, 1, 29).getDate() === 29;
-    if (leap) {
-      console.log("is leap year? " + leap);
-    } else {
-      console.log("is leap year? " + leap);
-    }
-    //max days if leap year
-    if (leap === true) {
-      var maxdays = "366";
-    } else {
-      var maxdays = "365";
-    }
+      //leap year check
+      let leap = new Date(data.wib[0].year, 1, 29).getDate() === 29;
+      //max days if leap year
+      if (leap) {
+        maxdays = "366";
+      } else {
+        maxdays = "365";
+      }
 
-    //
-    //
-    //time start (1 january ××××)
-    var date1 = new Date("01/01/" + data.wib[0].year);
-    //time now
-    var date2 = new Date(serverdate);
-    //get time of hour
-    var timeGet = date2.getTime() - date1.getTime();
-    //set into days
-    var Difference_In_Days = timeGet / (1000 * 3600 * 24);
+      //
+      //
+      //time start (1 january ××××)
+      var date1 = new Date("01/01/" + data.wib[0].year);
+      //time now
+      var date2 = new Date(serverdate);
+      //get time of hour
+      var timeGet = date2.getTime() - date1.getTime();
+      //set into days
+      var Difference_In_Days = timeGet / (1000 * 3600 * 24);
 
-    //progress line
-    const progress = document.getElementById("progress");
-    //percentage of days
-    const percent = document.getElementById("days_percentage");
-    //disable decimals of days total
-    const total = Difference_In_Days + 1;
+      //progress line
+      const progress = document.getElementById("progress");
+      //percentage of days
+      const percent = document.getElementById("days_percentage");
+      //disable decimals of days total
+      const total = Difference_In_Days + 1;
 
-    //set timeout for declaring progress and % after 2s if not loaded
-    const timeout = setTimeout(function () {
       //information of %
       percent.innerHTML = Math.trunc((total / maxdays) * "100") + "%";
       //year
       document.getElementById("year").innerHTML = serveryear;
       //progress
-      progress.style.width = Math.trunc((total / "365") * "100") - 1 + "%";
-      //Timeout time
-    }, 2000);
+      progress.style.width = Math.trunc((total / maxdays) * "100") - 1 + "%";
 
-    //
-    //
+      //
+      //
 
-    //extra information
-    var date_decimal = (total / "365") * "100";
-    document.getElementById("days_percentage_decimals").innerHTML =
-      date_decimal.toFixed(2);
+      //extra information
+      var date_decimal = (total / "365") * "100";
+      document.getElementById("days_percentage_decimals").innerHTML =
+        date_decimal.toFixed(2);
 
-    document.getElementById("days_total").innerHTML = total;
-    document.getElementById("maxdays").innerHTML = maxdays;
+      document.getElementById("days_total").innerHTML = total;
 
-    document.getElementById("servertime").innerHTML = servertime;
+      document.getElementById("maxdays").innerHTML = maxdays;
 
-    document.getElementById("servertime12H").innerHTML =
-      servertime12h + " " + data.wib[0].AMPM.toUpperCase();
+      document.getElementById("servertime").innerHTML = servertime;
 
-    document.getElementById("servertime24H").innerHTML = servertime24h;
-  });
-      
+      document.getElementById("servertime12H").innerHTML =
+        servertime12h + " " + data.wib[0].AMPM.toUpperCase();
+
+      document.getElementById("servertime24H").innerHTML = servertime24h;
+    });
+}, 1000);
