98dcc01909b0814352895db8c056d07bcb3e9900
Minin <70429604+MininxD@users.noreply.github.com>
2024-01-06 18:39:15 +0700

    Add files via upload

--- fetchTime.js
+++ fetchTime.js
@@ -21,6 +21,7 @@ export function waktu() {
 
 var dateOngoing = Number(data.wib[0].unixtime);
 var yearOngoing = Number(data.wib[0].year);
+thisYear.innerHTML = yearOngoing;
 // console.log(yearOngoing);
 
 var dateStart = new Date("January 1," + yearOngoing + ", 00:00:00");
